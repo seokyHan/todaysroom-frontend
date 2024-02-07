6802aa754cf15f8190c90fbcb0f9a71acf8c9e88
seokyHan <sseok1979@naver.com>
2024-02-07 18:03:11 +0900

    add: main map page 추가

---
+++ src/components/search/MapMain.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="map__main">
+		<div class="main__side-bar">
+			<div class="side-bar__header">
+				<span v-if="!isSelected">검색된 아파트 {{ getAptList.length }}개</span>
+				<button v-else @click="BACK_TO_ITEM_LIST" class="header__back-btn">
+					<AIcon type="arrow-left" />
+				</button>
+			</div>
+			<div class="side-bar__content">
+				<template v-if="loading">
+					<div class="side-bar__loading-spin">
+						<ASpin size="large" tip="Loading..." />
+					</div>
+				</template>
+				<template v-else>
+					<div v-if="!isSelected">
+						<ul
+							class="content__list"
+							v-if="getAptList.length !== 0"
+							key="app-list"
+						>
+							<AptItem
+								v-for="(apt, index) in getAptList"
+								:key="apt.aptCode"
+								:apt="apt"
+								:index="index"
+							/>
+						</ul>
+						<div v-else key="app-list" class="content_no-list">
+							<AIcon type="home" />
+							<h1>검색된 결과가 없습니다.</h1>
+						</div>
+					</div>
+					<div v-else class="content__detail">
+						<div class="detail__image">
+							<img
+								v-show="!roadViewStatus"
+								:src="getSelectedItem.img"
+								:alt="getSelectedItem.aptName"
+							/>
+							<div v-show="roadViewStatus" id="roadview"></div>
+							<button
+								v-if="!roadViewStatus"
+								class="image__road-view-btn image__road-view-btn--on"
+								@click="OnRoadView(getSelectedItem.lat, getSelectedItem.lng)"
+							>
+								<a-icon type="environment" /> 로드뷰 보기
+							</button>
+							<button
+								v-else
+								class="image__road-view-btn image__road-view-btn--off"
+								@click="OffRoadView"
+							>
+								<AIcon type="undo" /> 사진으로 되돌아가기
+							</button>
+						</div>
+						<div class="detail__header">
+							<h1 class="header__apt-name">{{ getSelectedItem.aptName }}</h1>
+							<p class="header__buildYear">
+								건축연도: {{ getSelectedItem.buildYear }}년
+							</p>
+						</div>
+						<div class="detail__main">
+							<p class="main__price">
+								매매 {{ getSelectedItem.recentPrice | convertAptPrice }}
+							</p>
+							<p class="main__address">
+								{{ getSelectedItem.sidoName }} {{ getSelectedItem.gugunName }}
+								{{ getSelectedItem.dongName }} {{ getSelectedItem.jibun }}
+							</p>
+						</div>
+						<div class="detail__ad">
+							<Advertisement />
+						</div>
+					</div>
+				</template>
+			</div>
+		</div>
+		<div class="main__map">
+			<KakaoMap ref="kakaoMap" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
