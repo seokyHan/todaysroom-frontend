e41fb40caa33409db4f5c677b5f684c53bde76f3
seokyHan <sseok1979@naver.com>
2024-05-20 18:20:45 +0900

    fix: accessToken state 수정

--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -2,31 +2,26 @@
 	<div class="signup-container">
 		<TermsForm @submitTerms="passSignupForm" v-if="!isCheckedTerms" />
 		<SignupForm v-else />
-		<!-- <OuathSignupForm v-else /> -->
 	</div>
 </template>
 
 <script>
 import TermsForm from '@/components/user/TermsForm.vue';
 import SignupForm from '@/components/user/SignupForm.vue';
-// import OuathSignupForm from '@/components/user/OauthSignupForm.vue';
 
 export default {
 	components: {
 		TermsForm,
 		SignupForm,
-		// OuathSignupForm,
 	},
 	data() {
 		return {
 			isCheckedTerms: false,
-			// isOauth: false,
 		};
 	},
 	methods: {
 		passSignupForm() {
 			this.isCheckedTerms = true;
-			// this.isOauth = oauthStatus === true ? true : false;
 		},
 	},
 };
