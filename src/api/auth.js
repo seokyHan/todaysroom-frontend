346cb9cb7a017e38f1356e323fbaab378e672de0
seokyHan <sseok1979@naver.com>
2024-05-22 18:13:34 +0900

    fix: credential 전역 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,7 @@ function signupUser(userData) {
 }
 
 function socialUserSignup() {
-	return instanceWithAuth.post('/users/social/signup', {withCredentials: true});
+	return instanceWithAuth.post('/users/social/signup');
 }
 
 function checkDuplicate(email) {
@@ -15,11 +15,11 @@ function checkDuplicate(email) {
 function userLogin(userData) {
 	// 다른 도메인에 요청과 응답에 쿠키를 허용하려면 withCredentials true 설정(로컬 개발 테스트용)
 	// 쿠키와 같은 인증과 관련된 데이터를 함부로 요청 데이터에 담지 않도록 되어서 withCredentials은 나중에 수정(backend도 포함)
-	return instance.post('/users/login', userData, {withCredentials: true});
+	return instance.post('/users/login', userData);
 }
 
 function reissue() {
-	return instance.post('/users/reissue', {withCredentials: true});
+	return instance.post('/users/reissue');
 }
 
 function userLogOut(logoutUserData) {
